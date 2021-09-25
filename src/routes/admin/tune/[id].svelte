9929605054a4d7c8d843e16f687a7777fd967632
tempakyousuke <tempakyousuke@gmail.com>
2021-09-26 07:51:22 +0900

    create tune page

---
+++ src/routes/admin/tune/[id].svelte
@@ -0,0 +1,26 @@
+<script context="module">
+	import { db } from '$modules/firebase/firebase';
+	import { doc, getDoc } from 'firebase/firestore';
+	export async function load({ page }) {
+		const tuneDoc = await getDoc(doc(db, 'tunes', page.params.id));
+
+		return {
+			props: {
+				tune: tuneDoc.data()
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import type { Tune } from '$types/tune';
+	export let tune: Tune;
+</script>
+
+<svelte:head>
+	<title>{tune.name}</title>
+</svelte:head>
+
+<div class="min-w-md flex justify-center items-center self-center pt-20">
+	<div class="flex flex-col px-4 py-6 bg-white rounded-lg shadow-md w-full max-w-md" />
+</div>
