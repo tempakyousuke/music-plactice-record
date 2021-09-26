1b62f9be5a0a06c8488868f84b99f8d6891e277d
tempakyousuke <tempakyousuke@gmail.com>
2021-09-26 17:29:56 +0900

    enable to view preview audio

--- src/routes/admin/tune/[id].svelte
+++ src/routes/admin/tune/[id].svelte
@@ -13,8 +13,19 @@
 </script>
 
 <script lang="ts">
+	import Button from '$lib/button/Button.svelte';
 	import type { Tune } from '$types/tune';
 	export let tune: Tune;
+	let fileinput;
+	let previewUrl;
+	const onFileSelected = (e) => {
+		let music = e.target.files[0];
+		let reader = new FileReader();
+		reader.readAsDataURL(music);
+		reader.onload = (e) => {
+			previewUrl = e.target.result;
+		};
+	};
 </script>
 
 <svelte:head>
@@ -22,5 +33,25 @@
 </svelte:head>
 
 <div class="min-w-md flex justify-center items-center self-center pt-20">
-	<div class="flex flex-col px-4 py-6 bg-white rounded-lg shadow-md w-full max-w-md" />
+	<div class="flex flex-col px-4 py-6 bg-white rounded-lg shadow-md w-full max-w-md">
+		<h1>ファイルアップロード</h1>
+		<Button
+			on:click={() => {
+				fileinput.click();
+			}}
+		>
+			音声ファイルを選択
+		</Button>
+		<input
+			style="display:none"
+			type="file"
+			accept="audio/*"
+			on:change={(e) => onFileSelected(e)}
+			bind:this={fileinput}
+		/>
+		<audio controls src={previewUrl}>
+			Your browser does not support the
+			<code>audio</code> element.
+		</audio>
+	</div>
 </div>
