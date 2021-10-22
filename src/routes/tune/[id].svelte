46c479121e2987932fe3169fe4112f519a933d45
tempakyousuke <tempakyousuke@gmail.com>
2021-10-22 16:05:10 +0900

    change button style

--- src/routes/tune/[id].svelte
+++ src/routes/tune/[id].svelte
@@ -51,9 +51,14 @@
 	<div class="max-w-lg ml-20 py-5">
 		<h1 class="text-3xl">
 			{tune.name}
-			<a class="ml-2 rounded-2xl hover:bg-gray-300 p-1 bg-gray-200 text-sm" href={tune.sessionLink}>
-				view the session page</a
-			>
+			{#if tune.sessionLink}
+				<a
+					class="ml-2 rounded-2xl hover:bg-gray-300 p-2 px-4 bg-gray-200 text-sm"
+					href={tune.sessionLink}
+				>
+					view the session page
+				</a>
+			{/if}
 		</h1>
 		<audio
 			class="mt-5 bg-white"
@@ -68,7 +73,7 @@
 		</audio>
 	</div>
 	{#each tune.records as record (record.id)}
-		<div class="cursor-pointer hover:bg-blue-100 py-5" on:click={() => selectRecord(record)}>
+		<div class="cursor-pointer hover:bg-blue-200 py-5" on:click={() => selectRecord(record)}>
 			<div class="mx-20 flex">
 				<div class="rounded-3xl bg-gray-100 w-8 flex justify-center content-center">
 					{#if record.id === selectedRecord?.id && !paused}
